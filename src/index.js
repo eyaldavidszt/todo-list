86a366df524bedaf6626fa5f4cecd1476b2f0eef
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-07-04 21:10:32 +0300

    Make initial form processing function

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import { translateToDOM } from "./dom";
 import './static/style.css'
 
 
-const btn = document.querySelector('button')
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
+function processFormInput(event) {
+    //makes card object from form input using cardMaker function 
+    //makes dom object from card object using translatetoDOM function
+    event.preventDefault();
     const titleInput = document.querySelector('input#title')
     const titleName = titleInput.value
+    if (!titleName) return
     titleInput.value = ''
-    const obj = cardMaker(titleName);
+    const obj = cardMaker(titleName)
     const DOMCard = translateToDOM(obj)
     document.body.appendChild(DOMCard)
-})
+}
+
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', processFormInput)
+
