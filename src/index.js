3aeb63adf076c5261b89e78194af71a2ef68bcec
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-07-04 21:47:43 +0300

    Create lists on main page

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ function processFormInput(event) {
     //makes card object from form input using cardMaker function 
     //makes dom object from card object using translatetoDOM function
     event.preventDefault();
-    const titleInput = document.querySelector('input#title')
+    const titleInput = document.querySelector('input#task-title')
     const titleName = titleInput.value
     if (!titleName) return
     titleInput.value = ''
     const obj = cardMaker(titleName)
     const DOMCard = translateToDOM(obj)
-    document.body.appendChild(DOMCard)
+
+    //shouldn't append to body though! it should append to fitting container which would be dynamic
+    const projects = document.querySelector('.projects')
+    projects.appendChild(DOMCard)
 }
 
 
-const btn = document.querySelector('button')
-btn.addEventListener('click', processFormInput)
+const makeCardBtn = document.querySelector('button.make-card-btn')
+makeCardBtn.addEventListener('click', processFormInput)
 
