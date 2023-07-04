43892bb48a658819beac7b6b10d384e038cbbef6
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-07-04 22:39:00 +0300

    Create initial categorization for tasks

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { translateToDOM } from "./dom";
 import './static/style.css'
 
 
-function processFormInput(event) {
+function processCardFormInput(event) {
     //makes card object from form input using cardMaker function 
     //makes dom object from card object using translatetoDOM function
     event.preventDefault();
@@ -15,11 +15,27 @@ function processFormInput(event) {
     const DOMCard = translateToDOM(obj)
 
     //shouldn't append to body though! it should append to fitting container which would be dynamic
-    const projects = document.querySelector('.projects')
+    const projects = document.querySelector('.list-wrapper')
     projects.appendChild(DOMCard)
 }
 
 
 const makeCardBtn = document.querySelector('button.make-card-btn')
-makeCardBtn.addEventListener('click', processFormInput)
+makeCardBtn.addEventListener('click', processCardFormInput)
+
+
+const makeListBtn = document.querySelector('button.make-list-btn')
+makeListBtn.addEventListener('click', processListFormInput)
+
+function processListFormInput(event) {
+    event.preventDefault()
+    const listInput = document.querySelector('input#list-title')
+    const listName = listInput.value
+    listInput.value = ''
+    if (!listName) return
+    const newList = document.createElement('div')
+    newList.classList.add('list-wrapper')
+    newList.innerHTML = `${listName}: `
+    document.body.appendChild(newList)
+}
 
