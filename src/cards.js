9dbbf3557de2f984298161161edd51d51717d98c
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-07-04 20:54:00 +0300

    Add initial updateTitle function for to-do card

--- src/cards.js
+++ src/cards.js
@@ -1,5 +1,9 @@
 export function cardMaker(title, description, dueDate, priority, status) {
-    return {title, description, dueDate, priority, status};
+    const newCard = {title, description, dueDate, priority, status, updateTitle}
+    function updateTitle(newName) {
+        newCard.title = newName
+    }
+    return newCard
 }
 
 
