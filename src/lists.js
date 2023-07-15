a6755ed23c4a7bb413f460b6d3e1bae4a6658579
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-07-15 18:04:17 +0300

    Unnest todo's from their categories in page

--- src/lists.js
+++ src/lists.js
@@ -3,7 +3,6 @@ export function listMaker({name}) {
 
     
     function createElement() {
-            
 
         const listWrapper = document.createElement('div')
         listWrapper.classList.add('list-wrapper')
