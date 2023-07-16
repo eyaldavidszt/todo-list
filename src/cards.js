d03d23036f2541a84fa6ff3251d4cc7714504a96
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-07-16 15:23:41 +0300

    Make template todo-edit form dialog

--- src/cards.js
+++ src/cards.js
@@ -21,38 +21,72 @@ export function cardMaker({title, description, dueDate, priority, status, parent
         const titleWrapper = document.createElement('div')
         titleWrapper.classList.add('task-title')
         titleWrapper.innerHTML = `${newCard.title}`
+        cardWrapper.classList.add(`${newCard.title}`)
         cardWrapper.appendChild(titleWrapper)
         const btn = document.createElement('button')
-        btn.innerHTML = 'delete todo'
+        btn.classList.add('todo-delete-btn')
+        btn.innerHTML = 'delete'
         btn.addEventListener('click', () => {
             removeElement(cardWrapper)
+        })
+        const editBtn = document.createElement('button')
+        editBtn.classList.add('todo-edit-btn')
+        editBtn.innerHTML = 'edit'
+        editBtn.addEventListener('click',  () => {
+            //create form dynamically
+            //submitting the form triggers 'editElement' function which edits obj, dom, and localStorage
+            const editDialog = document.createElement('dialog')
+            const editForm = document.createElement('form')
+            editForm.setAttribute('method', 'dialog')
+            //  create children for form:
+                const editFormLabel = document.createElement('label')
+                editFormLabel.setAttribute('for', 'task-title-edit')
+                const editFormInput = document.createElement('input')
+                editFormInput.setAttribute('type', 'text')
+                editFormInput.setAttribute('id', 'task-title-edit')
+                editFormInput.setAttribute('placeholder', 'title')
+                editFormInput.setAttribute('autocomplete', 'off')
+                //add more inputs like date, importance
+                const editFormSubmit = document.createElement('button')
+                editFormSubmit.setAttribute('class', 'edit-todo-btn')
+                editFormSubmit.innerHTML = 'submit'
+                editFormSubmit.addEventListener('click', () => {
+                    //function to process form and send it into editElement()
+                    
+                })
+
+            //
+            editDialog.appendChild(editForm)
+            document.body.appendChild(editDialog)
+            editDialog.showModal()
+
+            editDialog.addEventListener("click", e => {
+                const dialogDimensions = editDialog.getBoundingClientRect()
+                if (
+                  e.clientX < dialogDimensions.left ||
+                  e.clientX > dialogDimensions.right ||
+                  e.clientY < dialogDimensions.top ||
+                  e.clientY > dialogDimensions.bottom
+                ) {
+                  editDialog.remove()
+                }
+              })
         })
         const parentDiv = document.createElement('div')
         parentDiv.textContent = parent
         parentDiv.classList.add('parent')
         cardWrapper.appendChild(parentDiv)
         cardWrapper.appendChild(btn)
+        cardWrapper.appendChild(editBtn)
         //append other stuff, maybe in internal functions.
 
         appendToLocal()
-        //
-        // const nodes = document.querySelectorAll('.list-wrapper')
-        // let parentNode
-        // for (let node of nodes) {
-        //     if (node.innerHTML.trim().includes(parent)) {
-        //         parentNode = node
-        //     }
-        // }
-        // if (!parentNode) {
-        //     if (localStorage.getItem(`${parent} list`)) {
-        //         parentNode = listMaker({name: parent}).createElement()
-        //     }
-        //     else return    
-        // }
-        // parentNode.appendChild(cardWrapper)
-        //
         const todosWrapper = document.querySelector('.todos-wrapper')
         todosWrapper.appendChild(cardWrapper)
+    }
+
+    function editElement() {
+        
     }
 
 
